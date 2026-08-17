02bb5bad5a62d99e72967039f835545eb0ceca25
Bruno Valadão Cunha <brunovcunha@hotmail.com>
2026-08-17 00:10:51 -0300

    Alright ExternalTool detecting, window opening, Yoshi icons appearing

--- YoshisIsland_BizHawkTool/ToolForm.cs
+++ YoshisIsland_BizHawkTool/ToolForm.cs
@@ -1,7 +1,53 @@
-﻿namespace YoshisIsland_BizHawkTool
+﻿using BizHawk.Client.Common;
+using BizHawk.Client.EmuHawk;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace YoshisIsland_BizHawkTool
 {
-    public class ToolForm
+    [ExternalTool("Yoshi's Island Utilities", Description = "Yoshi's Island (SNES) Utilities for BizHawk 2.10+")] // this appears in the Tools > External Tools submenu in EmuHawk
+    [ExternalToolEmbeddedIcon("YoshisIsland_BizHawkTool.Resources.Images.yoshi_icon_16px.png")]
+    public class ToolForm : ToolFormBase, IExternalToolForm
     {
-        
+        // CONSTANTS
+        public const string TOOL_NAME = "YoshisIsland_BizHawkTool";
+        public const string WINDOW_TITLE = "Yoshi's Island Utilities Tool";
+
+
+        // PROPERTIES
+        protected override string WindowTitleStatic => WINDOW_TITLE;
+
+
+        // CONSTRUCTOR
+        public ToolForm()
+        {
+            ClientSize = new Size(480, 320);
+            SuspendLayout();
+            Controls.Add(new Label { AutoSize = true, Text = "Yoshi!!" });
+            ResumeLayout(performLayout: false);
+            PerformLayout();
+            this.Icon = Properties.Resources.yoshi_icon_16px;
+        }
+
+        // METHODS
+        public override void Restart()
+        {
+            base.Restart();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToolForm));
+            this.SuspendLayout();
+            // 
+            // ToolForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ToolForm";
+            this.ResumeLayout(false);
+
+        }
     }
 }
