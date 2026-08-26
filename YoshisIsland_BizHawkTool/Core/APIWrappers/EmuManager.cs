820f4f1e292dc4a81c4c69bdff834d80ba20042d
Bruno Valadão Cunha <brunovcunha@hotmail.com>
2026-08-25 22:24:24 -0300

    Start of the EmuManager, for now it detects if a core is loaded

---
+++ YoshisIsland_BizHawkTool/Core/APIWrappers/EmuManager.cs
@@ -0,0 +1,31 @@
+﻿using BizHawk.Client.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoshisIsland_BizHawkTool
+{
+    internal static class EmuManager
+    {
+        private const string NULL_SYSTEM_ID = "NULL";
+        private const string CORE_EXCEPTION_MESSAGE = "This external tool should be used with a core loaded. Open the game ROM and run the tool again.";
+
+        private static IEmulationApi _emuAPI;
+        private static ToolOptions _options;
+
+        internal static void Init(IEmulationApi emuAPI)
+        {
+            _emuAPI = emuAPI;
+            _options = ToolOptions.Instance;
+            CheckIfCoreLoaded();   
+        }
+
+        private static void CheckIfCoreLoaded()
+        {
+            if (_emuAPI.GetSystemId() == NULL_SYSTEM_ID)
+                throw new NoCoreLoadedException(CORE_EXCEPTION_MESSAGE);
+        }
+    }
+}
