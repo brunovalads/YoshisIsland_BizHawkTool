8460186d7cad4dd14c670d95b535d2a248c288bd
Bruno Valadão Cunha <brunovcunha@hotmail.com>
2026-09-20 21:37:17 -0300

    Abstraction of memory address, to make it way easier to catalog the addresses needed

---
+++ YoshisIsland_BizHawkTool/Core/Emu/Data/MemoryAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BizHawk.Client.Common;
+
+namespace YoshisIsland_BizHawkTool
+{
+    internal class MemoryAddress
+    {
+        internal int Address { get; }
+        internal int Size { get; }
+        internal MemoryDomain Domain { get; }
+
+        internal MemoryAddress(int address, int size, MemoryDomain domain)
+        {
+            Address = address;
+            Size = size;
+            Domain = domain;
+        }
+    }
+}
